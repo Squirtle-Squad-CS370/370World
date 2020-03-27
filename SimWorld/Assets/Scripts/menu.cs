9fb3cc782055ecbb152047ac77966fe28890c086
Paulo-Rendon <rendonp@sonoma.edu>
2020-03-26 22:27:30 -0700

    work in progress option menu, quit button now closes app, new game now runs test scene.

---
+++ SimWorld/Assets/Scripts/menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    public void PlayGameEasy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGameMedium()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGameHard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("READ IF QUIT COMMAND REACHED");
+        Application.Quit();
+    }
+}
