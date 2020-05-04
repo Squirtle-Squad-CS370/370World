9dd5cc66f8a08e80856f87dda3121fb1b5e6a083
Paulosito Rendonito <Rendonp@sonoma.edu>
2020-05-04 16:12:55 -0700

    Working pause menu

---
+++ SimWorld/Assets/Scripts/pause_menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause_menu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void main_menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
